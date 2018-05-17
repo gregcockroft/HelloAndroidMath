071d6e22a4a8b0de201af1552397031893be297b
Greg Cockroft <greg@activbody.com>
2018-05-17 19:24:51 -0400

    Initial commit

---
+++ app/src/main/java/com/agog/hellomath/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.agog.hellomath
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.agog.mathdisplay.MTMathView
+import com.agog.mathdisplay.parse.MTParseError
+import com.agog.mathdisplay.parse.MTParseErrors
+import junit.framework.Assert.assertEquals
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // We are going to not display parse errors visibly
+        mathview.displayErrorInline = false
+
+        mathview.latex = "2+1"
+        mathview.fontSize = MTMathView.convertDpToPixel(40.0f)
+
+        // Check to make sure the latex string was well formatted
+        assertEquals("Parse Error", MTParseErrors.ErrorNone, mathview.lastError.errorcode)
+
+    }
+}
